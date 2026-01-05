7fa3d8f279c155117e42a24f43737a5f49f3a878
granitaistore-prog <granit.ai.store@gmail.com>
2026-01-05 12:47:08 +0200

    Update MainActivity.kt

--- app/src/main/java/com/granitaistore/obd_diagnostic_v2/MainActivity.kt
+++ app/src/main/java/com/granitaistore/obd_diagnostic_v2/MainActivity.kt
@@ -1,1 +1,10 @@
-class MainActivity{}
+package com.granitaistore.obd_diagnostic_v2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
