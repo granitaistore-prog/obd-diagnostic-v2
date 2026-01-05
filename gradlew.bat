7760078a172d8df0bc4a3245e7f6d4b1f62b639f
granitaistore-prog <granit.ai.store@gmail.com>
2026-01-05 12:35:13 +0200

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,3 +1,5 @@
 @echo off
 set DIR=%~dp0
-java -jar "%DIR%\gradle\wrapper\gradle-wrapper.jar" %*
+set CLASSPATH=%DIR%gradle\wrapper\gradle-wrapper.jar
+
+java -Xmx64m -Xms64m -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
